bfc389ba1b35f52263ec152e3418ad1209451bbc
Yuri <yuri.rage@gmailcom>
2021-09-02 12:19:25 -0500

    added BLINK_ON_SEND_SUCCESS

--- ESP-Now-Serial-Bridge.ino
+++ ESP-Now-Serial-Bridge.ino
@@ -11,11 +11,15 @@
  *   required for truly long range messaging, to combat obstacles/walls, and/or
  *   to achieve success in an area saturated with 2.4GHz traffic.
  * 
- * I made heavy use of compiler macros to keep the sketch compact/efficient
+ * I made heavy use of compiler macros to keep the sketch compact/efficient.
  *
  * To find the MAC address of each board, uncomment the #define DEBUG line, 
  *   and monitor serial output on boot.  Set the OPPOSITE board's IP address
  *   as the value for RECVR_MAC in the macros at the top of the sketch.
+ *   
+ * The BLINK_ON_* macros should be somewhat self-explanatory.  If your board has a built-in
+ *   LED (or you choose to wire an external one), it can indicate ESP-Now activity as
+ *   defined by the macros you choose to enable.
  *
  * When uploading the sketch, be sure to define BOARD1 or BOARD2 as appropriate
  *   before compiling.
@@ -35,15 +39,17 @@
 #include <WiFi.h>
 #include <esp_wifi.h>
 
-#define BOARD2 // BOARD1 or BOARD2
+#define BOARD1 // BOARD1 or BOARD2
 
 #ifdef BOARD1
 #define RECVR_MAC {0x7C, 0x9E, 0xBD, 0x47, 0x50, 0x30}  // replace with your board's address
 //#define BLINK_ON_SEND
+//#define BLINK_ON_SEND_SUCCESS
 #define BLINK_ON_RECV
 #else
 #define RECVR_MAC {0x94, 0xB9, 0x7E, 0xD2, 0xF7, 0x54}  // replace with your board's address
-#define BLINK_ON_SEND
+//#define BLINK_ON_SEND
+#define BLINK_ON_SEND_SUCCESS
 //#define BLINK_ON_RECV
 #endif
 
@@ -70,13 +76,31 @@ uint8_t buf_send[BUFFER_SIZE];
 uint8_t buf_size = 0;
 uint32_t send_timeout = 0;
 
+#if defined(DEBUG) || defined(BLINK_ON_SEND_SUCCESS)
+uint8_t led_status = 0;
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  #ifdef DEBUG
   if (status == ESP_NOW_SEND_SUCCESS) {
     Serial.println("Send success");
+  } else {
+  Serial.println("Send failed");
+  }
+  #endif
+
+  #ifdef BLINK_ON_SEND_SUCCESS
+  if (status == ESP_NOW_SEND_SUCCESS) {
+    led_status = ~led_status;
+    // this function happens too fast to register a blink
+    // instead, we latch on/off as data is successfully sent
+    digitalWrite(LED_BUILTIN, led_status);
     return;
   }
-  Serial.println("Send failed");
+  // turn off the LED if send fails
+  led_status = 0;
+  digitalWrite(LED_BUILTIN, led_status);
+  #endif
 }
+#endif
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   #ifdef BLINK_ON_RECV
@@ -118,7 +142,7 @@ void setup() {
     return;
   }
 
-  #ifdef DEBUG
+  #if defined(DEBUG) || defined(BLINK_ON_SEND_SUCCESS)
   esp_now_register_send_cb(OnDataSent);
   #endif
   
