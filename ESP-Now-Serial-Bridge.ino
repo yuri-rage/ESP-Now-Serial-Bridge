c2c09fc24339e399f55e57c866c79c210a065e7b
Thomas Sosio <tomy983@gmail.com>
2024-01-13 12:24:59 +0100

    Update ESP-Now-Serial-Bridge.ino
    Added ESP8266 support

--- ESP-Now-Serial-Bridge.ino
+++ ESP-Now-Serial-Bridge.ino
@@ -1,7 +1,7 @@
 /*********************************************************************************
  * ESP-Now-Serial-Bridge
  *
- * ESP32 based serial bridge for transmitting serial data between two boards
+ * ESP8266/ESP32 based serial bridge for transmitting serial data between two boards
  *
  * The primary purpose of this sketch was to enable a MAVLink serial connection,
  *   which I successfully tested at 57600 bps.  In theory, much faster buad rates
@@ -24,7 +24,8 @@
  * When uploading the sketch, be sure to define BOARD1 or BOARD2 as appropriate
  *   before compiling.
  *
- * -- Yuri - Sep 2021
+ * -- Yuri - Sep 2021 
+ * -- Thomas -Dec 2022
  *
  * Based this example - https://randomnerdtutorials.com/esp-now-two-way-communication-esp32/
  *
@@ -35,9 +36,17 @@
  * copies or substantial portions of the Software.
 *********************************************************************************/
 
+#define ESP8266 // ESP8266 or ESP32
+
+#ifdef ESP8266
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+#else
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
+#endif
 
 #define BOARD1 // BOARD1 or BOARD2
 
@@ -66,7 +75,7 @@
 #define LED_BUILTIN 2  // some boards don't have an LED or have it connected elsewhere
 #endif
 
-const uint8_t broadcastAddress[] = RECVR_MAC;
+uint8_t broadcastAddress[] = RECVR_MAC;
 // wait for double the time between bytes at this serial baud rate before sending a packet
 // this *should* allow for complete packet forming when using packetized serial comms
 const uint32_t timeout_micros = (int)(1.0 / BAUD_RATE * 1E6) * 20;
@@ -76,13 +85,19 @@ uint8_t buf_send[BUFFER_SIZE];
 uint8_t buf_size = 0;
 uint32_t send_timeout = 0;
 
+#ifndef ESP8266
 esp_now_peer_info_t peerInfo;  // scope workaround for arduino-esp32 v2.0.1
+#endif
 
 #if defined(DEBUG) || defined(BLINK_ON_SEND_SUCCESS)
 uint8_t led_status = 0;
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   #ifdef DEBUG
+  #ifdef ESP8266
+  if (sendStatus == 0) {
+  #else
   if (status == ESP_NOW_SEND_SUCCESS) {
+  #endif
     Serial.println("Send success");
   } else {
   Serial.println("Send failed");
@@ -90,7 +105,11 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   #endif
 
   #ifdef BLINK_ON_SEND_SUCCESS
+  #ifdef ESP8266
+  if (sendStatus == 0) {
+  #else
   if (status == ESP_NOW_SEND_SUCCESS) {
+  #endif
     led_status = ~led_status;
     // this function happens too fast to register a blink
     // instead, we latch on/off as data is successfully sent
@@ -104,7 +123,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 #endif
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   #ifdef BLINK_ON_RECV
   digitalWrite(LED_BUILTIN, HIGH);
   #endif
@@ -121,7 +140,11 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
  
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  #ifdef ESP8266
+  Serial.begin(BAUD_RATE);
+  #else
   Serial.begin(BAUD_RATE, SER_PARAMS, RX_PIN, TX_PIN);
+  #endif
   Serial.println(send_timeout);
   WiFi.mode(WIFI_STA);
 
@@ -130,30 +153,46 @@ void setup() {
   Serial.println(WiFi.macAddress());
   #endif
   
-  if (esp_wifi_set_channel(WIFI_CHAN, WIFI_SECOND_CHAN_NONE) != ESP_OK) {
+  #ifdef ESP8266
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  #else
+   if (esp_wifi_set_channel(WIFI_CHAN, WIFI_SECOND_CHAN_NONE) != ESP_OK) {
     #ifdef DEBUG
     Serial.println("Error changing WiFi channel");
     #endif
     return;
   }
-
+  #endif
+ 
+  #ifdef ESP8266
+  if (esp_now_init() != 0) {
+  #else
   if (esp_now_init() != ESP_OK) {
+  #endif
     #ifdef DEBUG
     Serial.println("Error initializing ESP-NOW");
     #endif
     return;
   }
+  #ifdef ESP8266
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO); // Set ESP-NOW Role
+  #endif
 
   #if defined(DEBUG) || defined(BLINK_ON_SEND_SUCCESS)
   esp_now_register_send_cb(OnDataSent);
   #endif
   
+  #ifdef ESP8266
+  if (esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0) != 0) {
+  #else
   // esp_now_peer_info_t peerInfo;  // scope workaround for arduino-esp32 v2.0.1
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = WIFI_CHAN;  
   peerInfo.encrypt = false;
 
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
+  #endif
     #ifdef DEBUG
     Serial.println("Failed to add peer");
     #endif
@@ -179,10 +218,18 @@ void loop() {
     #ifdef BLINK_ON_SEND
     digitalWrite(LED_BUILTIN, HIGH);
     #endif
+    #ifdef ESP8266
+    int result = esp_now_send(broadcastAddress, (uint8_t *) &buf_send, buf_size);
+    #else
     esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &buf_send, buf_size);
+    #endif  
     buf_size = 0;
     #ifdef DEBUG
+    #ifdef ESP8266
+    if (result != 0) {
+    #else
     if (result == ESP_OK) {
+    #endif  
       Serial.println("Sent!");
     }
     else {
